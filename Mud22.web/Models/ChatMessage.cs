4b774a11e07d04be6c75778de7beda8f036b150b
khanfan <khanfan@gmail.com>
2021-12-09 15:35:27 +0700

    Add project files.

---
+++ Mud22.web/Models/ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Mud22.web.Shared
+{
+    public class ChatMessage
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Mud22.web/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mud22.web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
